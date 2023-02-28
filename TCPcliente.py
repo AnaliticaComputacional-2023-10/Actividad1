d65ff3795b71576d4d307c8d30ee84249ff24288
SantiGonzalezz <s.gonzalez35@uniandes.edu.co>
2023-02-28 17:00:03 -0500

    Advance

--- TCPcliente.py
+++ TCPcliente.py
@@ -3,7 +3,7 @@ from socket import *
 servidorNombre = "127.0.0.1"
 servidorPuerto = 12000
 clienteSocket = socket(AF_INET, SOCK_STREAM)
-clienteSocket.connect((servidorNombre,servidorPuerto))
+clienteSocket.connect((servidorNombre, servidorPuerto))
 mensaje = input("Ingrese un mensaje:")
 clienteSocket.send(bytes(mensaje, "utf-8"))
 mensajeRespuesta = clienteSocket.recv(1024)
--- TCPservidor.py
+++ TCPservidor.py
@@ -1,14 +1,14 @@
 from socket import *
 
-servidorPuerto = 12000
-servidorSocket = socket(AF_INET,SOCK_STREAM)
-servidorSocket.bind(('',servidorPuerto))
+servidorPuerto = 8050
+servidorSocket = socket(AF_INET, SOCK_STREAM)
+servidorSocket.bind(('', servidorPuerto))
 servidorSocket.listen(1)
 print("El servidor está listo para recibir mensajes")
 while 1:
     conexionSocket, clienteDireccion = servidorSocket.accept()
     print("Conexión establecida con ", clienteDireccion)
-    mensaje = str( conexionSocket.recv(1024), "utf-8" )
+    mensaje = str(conexionSocket.recv(1024), "utf-8")
     print("Mensaje recibido de ", clienteDireccion)
     print(mensaje)
     mensajeRespuesta = mensaje.upper()
